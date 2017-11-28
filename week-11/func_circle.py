4698d83e76335fd2443482d0100dfe1853d4e0ea
Miko <mixeradamski@gmail.com>
2017-11-28 22:40:09 +0000

    labsheet19

---
+++ week-11/func_circle.py
@@ -0,0 +1,14 @@
+pi = 3.141
+
+def circumference(r):
+   return (2 * pi * r)
+
+def area(r):
+   return (pi * r ** 2)
+
+def main():
+   print circumference(5)
+   print area(5)
+
+if __name__ == "__main__":
+   main()
