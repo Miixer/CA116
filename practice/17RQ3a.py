b9eb410591301430a4f77d3320ebc52e631e144a
Miixer <33287805+Miixer@users.noreply.github.com>
2018-01-12 22:53:59 +0000

    .

--- practice/17RQ3a.py
+++ practice/17RQ3a.py
@@ -3,11 +3,8 @@ a = [1,2,3,4,5]
 i = 0
 while i < len(a) / 2:
    tmp = a[i]
-   print tmp
    a[i] = a[len(a) - i - 1]
-   print a[i]
-   a[len(a) - i - 1] = tmp
-   print tmp     
+   a[len(a) - i - 1] = tmp 
    i = i + 1
 
 print a
---
+++ practice/17RQ3b.py
@@ -0,0 +1,9 @@
+s = "hlo"
+t = ""
+
+i = 0
+while i < len(s):
+   t = s[i] + t
+   i = i + 1
+s = t
+print s
